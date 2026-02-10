d2499589ce17a2fbeed1a19eb64ec2c0232e8e55
dfcastro <dfcastro@outlook.com.br>
2026-02-09 22:42:06 -0300

    corrigindo botões de busca rápida

--- resources/views/livewire/gerir-processos.blade.php
+++ resources/views/livewire/gerir-processos.blade.php
@@ -720,9 +720,7 @@ with(fn() => [
                                                     <option>Arquivado</option>
                                                 </optgroup>
                                             </select>
-                                            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-4 text-slate-500">
-                                                <svg class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" /></svg>
-                                            </div>
+                                            
                                         </div>
                                     </div>
                                 </div>
